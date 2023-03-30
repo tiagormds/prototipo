236f605cb73eb44509440114b60980f0fb4e8fff
tiagorafael <tiagorafaelmelo@gmail.com>
2023-03-30 08:18:12 -0300

    Atualizações do design dos botôes

--- resources/views/property/index.blade.php
+++ resources/views/property/index.blade.php
@@ -1,7 +1,7 @@
 @extends('property.master')
 @section('content')
     <div class="container my-5">
-        <h1>listagem de produtos</h1>
+        <h1>Listagem de Produtos</h1>
 
         @if(!empty($properties))
             <table class="table table-striped table-hover">
@@ -18,14 +18,14 @@
                         <td>{{ $property->title }}</td>
                         <td>R$ {{ number_format($property->rental_price, 2, ',', '.')  }}</td>
                         <td>R$ {{ number_format($property->sale_price, 2, ',', '.')  }}</td>
-                        <td><a href="{{ route('imovel.show', $property->name) }}">Ver mais</a></td>
-                        <td><a href="{{ route('imovel.edit', $property->name) }}">Editar</a></td>
+                        <td><a class="btn btn-sm btn-success" href="{{ route('imovel.show', $property->name) }}">Ver mais</a></td>
+                        <td><a class="btn btn-sm btn-warning" href="{{ route('imovel.edit', $property->name) }}">Editar</a></td>
                         <td>
                             <form action="{{ route('imovel.destroy', $property->name) }}" method="post">
                                 @csrf
                                 @method('DELETE')
 
-                                <button type="submit">Remover</button>
+                                <button class="btn btn-sm btn-danger" type="submit">Remover</button>
                             </form>
                         </td>
                     </tr>
